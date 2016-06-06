269858db6eaeed117c6940ef037573700cc5fdfe
Juliankarl33 <bu43jazu@stud.informatik.uni-erlangen.de>
2016-06-06 18:03:11 +0200

    aufgabe4

--- waveguide.cpp
+++ waveguide.cpp
@@ -16,9 +16,7 @@ typedef struct{
 } point; 
 
 typedef struct{
-  uint16_t v0;
-  uint16_t v1;
-  uint16_t v2;
+  uint16_t v[3];
 } face; 
 
 //typedef double (*func_t)(double, double);
@@ -44,12 +42,16 @@ void readInput( point* points, face* faces ){
     // read faces
     std::getline(infile, line);
     std::getline(infile, line);
-    for(int j=0; j<= 1976; j++) 
+        std::getline(infile, line);
+
+
+    
+    for(int j=0; j< 1976; j++) 
     {
       infile >> v0 >> v1 >> v2;
-      faces[j].v0 = v0;
-      faces[j].v1 = v1;
-      faces[j].v2 = v2;
+      faces[j].v[0] = v0;
+      faces[j].v[1] = v1;
+      faces[j].v[2] = v2;
     }
     
     infile.close();
@@ -85,7 +87,7 @@ double my_func(double x, double y){
 }
 
 
-void computeA_M( point* points, face* faces, std::vector<std::map<double, uint16_t>> matrixA, std::vector<std::map<double, uint16_t>> matrixB){
+void computeA_M( point* points, face* faces, std::vector<std::map<uint16_t, double>>& matrixA, std::vector<std::map<uint16_t, double>>& matrixM){
 
     ELEMENTS::Triangle my_element;
     std::vector< std::vector< double > > my_local_matrixA;
@@ -93,24 +95,26 @@ void computeA_M( point* points, face* faces, std::vector<std::map<double, uint16
 
     std::vector<double> corners(6, 0.0);
 
-    for(int i=0; i<1977; i++){
-        corners[0] = points[faces[i].v0].x; corners[1] = points[faces[i].v0].y;
-        corners[2] = points[faces[i].v1].x; corners[3] = points[faces[i].v0].y;
-        corners[4] = points[faces[i].v2].x; corners[5] = points[faces[i].v0].y;
+    for(int i=0; i<1976; i++){
+        corners[0] = points[faces[i].v[0]].x; corners[1] = points[faces[i].v[0]].y;
+        corners[2] = points[faces[i].v[1]].x; corners[3] = points[faces[i].v[1]].y;
+        corners[4] = points[faces[i].v[2]].x; corners[5] = points[faces[i].v[2]].y;
         my_element(corners);
 
 
-        my_local_matrixA = my_element.integrate(grad(v_()) * grad(w_()));
-        my_local_matrixM = my_element.integrate(func<double>(my_func) * v_() * w_());
+        my_local_matrixA = my_element.integrate(grad(v_()) * grad(w_()) - func<double>(my_func)*w_()*v_());
+        my_local_matrixM = my_element.integrate( v_() * w_());
 
         for(int k=0; k<3; k++){
-            //hier habe ich noch den zweiten [] Operator hinzugefuegt, damit wir ein double von my-local-matrix erhalten
-                matrixA[faces[i].v0].insert ( std::pair<double,uint16_t>(my_local_matrixA[k*3][1],faces[i].v0) );
-                matrixB[faces[i].v0].insert ( std::pair<double,uint16_t>(my_local_matrixM[k*3][1],faces[i].v0) );
-                matrixA[faces[i].v1].insert ( std::pair<double,uint16_t>(my_local_matrixA[k*3+1][1],faces[i].v1) );
-                matrixB[faces[i].v1].insert ( std::pair<double,uint16_t>(my_local_matrixM[k*3+1][1],faces[i].v1) );
-                matrixA[faces[i].v2].insert ( std::pair<double,uint16_t>(my_local_matrixA[k*3+2][1],faces[i].v2) );
-                matrixB[faces[i].v2].insert ( std::pair<double,uint16_t>(my_local_matrixM[k*3+2][1],faces[i].v2) );
+	   for(int l=0; l<3; l++){
+	     if(faces[i].v[l] == 0 && faces[i].v[k] == 0 ){
+		std::cout << "i: " << i << " localmatrix : " << my_local_matrixA[0][0] << std::endl;
+	     }
+		matrixA[faces[i].v[l]][faces[i].v[k]] += my_local_matrixA[l][k];
+
+		matrixM[faces[i].v[l]][faces[i].v[k]] += my_local_matrixM[l][k];
+
+	   }
         }
 
     }
@@ -119,7 +123,43 @@ void computeA_M( point* points, face* faces, std::vector<std::map<double, uint16
 
 }
 
+void Jaccobi(  std::vector<std::map<uint16_t, double>>& &u,
+	       std::vector<std::map<uint16_t, double>>& matrixA,
+	       std::vector<std::map<uint16_t, double>>& u_neu, 
+	       std::vector<std::map<uint16_t, double>>& f, 
+	       double h, double epsilon){
 
+  double tmp = 0.0;
+  int count = 0;
+	for (int iterations = 0; iterations<numIterations; iterations++){
+		for (int m = 1; m<1039; m++){
+			for (std::map<uint16_t, double>::iterator it=matrixA[i].begin(); it!=matrixA[i].end(); ++it){
+			  tmp += it->second * u[it->first];
+			  count++;
+				//u_neu(q, m) = (1.0 / 4.0) * (h*h*f(q, m) + (u(q - 1, m) + u(q + 1, m) + u(q, m - 1) + u(q, m + 1)));
+			}
+			tmp += f[m];
+			u_neu[m] = tmp / count;
+			tmp =0.0;
+			count= 0;
+		}
+	}
+
+
+    
+
+}
+
+
+void printMat( std::string name, int dim, std::vector<std::map<uint16_t, double>>& matrix ){
+  std::ofstream out( name );
+
+  for(int i=0; i<dim; i++ ){
+    for (std::map<uint16_t, double>::iterator it=matrix[i].begin(); it!=matrix[i].end(); ++it){
+      out << i << " " << it->first << " " << it->second << std::endl;
+    }
+  }
+}
 
 
 int main( int args, char** argv ){
@@ -136,20 +176,23 @@ int main( int args, char** argv ){
   double k[1039];
 
   // matrices
-  std::vector< std::map<double, uint16_t> > matrixA(1039);
-  std::vector< std::map<double, uint16_t> > matrixM(1039);
+  std::vector< std::map<uint16_t, double> > matrixA(1039);
+  std::vector< std::map<uint16_t, double> > matrixM(1039);
 
   
   delta = atof( argv[1] );
   //epsilon = atof( argv[2] );
   
   readInput( points, faces );
-  
-  setK( points, k, delta );
 
- computeA_M( points, faces, matrixA, matrixM);
+   setK( points, k, delta );
+
+  computeA_M( points, faces, matrixA, matrixM);
+  printMat( std::string("A.txt"), 1039, matrixA );
+  printMat( std::string("M.txt"), 1039, matrixM );
   
-  testK( points, k );
+
+  //testK( points, k );
   exit( EXIT_SUCCESS );
   
 }
